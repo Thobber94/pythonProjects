b6c81ab44158e54221521980f912dbd7976abd57
Thomas Bergby <thomas.bergby@gmail.com>
2016-06-22 12:55:26 +0200

    Updated the code to now allow user input with tracing letters. - Can now write: wwwww or similar, as long as its starts with the correct letter. - This version might only work for one robot. You need to configure the direction of the motors on each indv. robot

--- LegoProjects/simplebot_simple.py
+++ LegoProjects/simplebot_simple.py
@@ -1,60 +1,88 @@
+# coding=utf-8
 from BrickPi import *
 
 
 # Move Forward
 def forover():
-    BrickPi.MotorSpeed[motor1] = hastighet
-    BrickPi.MotorSpeed[motor2] = hastighet
+    BrickPiUpdateValues()
+    BrickPi.MotorSpeed[motor1] = -hastighet
+    BrickPi.MotorSpeed[motor2] = -hastighet
+    BrickPi.MotorSpeed[motor3] = hastighet
+    BrickPi.MotorSpeed[motor4] = hastighet
+    BrickPiUpdateValues()
 
 
 # Move Left
 def venstre():
-    BrickPi.MotorSpeed[motor1] = hastighet
-    BrickPi.MotorSpeed[motor2] = -hastighet
+    BrickPi.MotorSpeed[motor2] = hastighet
+    BrickPi.MotorSpeed[motor3] = -hastighet
+    BrickPi.MotorSpeed[motor1] = -hastighet
+    BrickPi.MotorSpeed[motor4] = hastighet
+    BrickPiUpdateValues()
 
 
 # Move Right
 def hoyre():
-    BrickPi.MotorSpeed[motor1] = -hastighet
-    BrickPi.MotorSpeed[motor2] = hastighet
+    BrickPi.MotorSpeed[motor2] = -hastighet
+    BrickPi.MotorSpeed[motor3] = hastighet
+    BrickPi.MotorSpeed[motor1] = hastighet
+    BrickPi.MotorSpeed[motor4] = -hastighet
+    BrickPiUpdateValues()
 
 
 # Move backward
 def revers():
-    BrickPi.MotorSpeed[motor1] = -hastighet
-    BrickPi.MotorSpeed[motor2] = -hastighet
+    BrickPi.MotorSpeed[motor1] = hastighet
+    BrickPi.MotorSpeed[motor2] = hastighet
+    BrickPi.MotorSpeed[motor3] = -hastighet
+    BrickPi.MotorSpeed[motor4] = -hastighet
+    BrickPiUpdateValues()
 
 
 # Stop
 def stop():
     BrickPi.MotorSpeed[motor1] = 0
     BrickPi.MotorSpeed[motor2] = 0
+    BrickPi.MotorSpeed[motor3] = 0
+    BrickPi.MotorSpeed[motor4] = 0
 
 
 BrickPiSetup()  # setup the serial port for communication
 
 motor1 = PORT_B
 motor2 = PORT_C
-BrickPi.MotorEnable[motor1] = 1  # Enable the Motor A
-BrickPi.MotorEnable[motor2] = 1  # Enable the Motor B
+motor3 = PORT_D
+motor4 = PORT_A
+BrickPi.MotorEnable[motor1] = 1  # Enable the Motor B
+BrickPi.MotorEnable[motor2] = 1  # Enable the Motor C
+BrickPi.MotorEnable[motor3] = 1  # Enable Motor D
+BrickPi.MotorEnable[motor4] = 1  # Enable Motor A
 BrickPiSetupSensors()  # Send the properties of sensors to BrickPi
 BrickPi.Timeout = 10000  # Set timeout value for the time till which to run the motors after the last command is pressed
 BrickPiSetTimeout()  # Set the timeout
 
-hastighet = 125  # Setter hastighet. (Sett en hastighet mellom -255 og 255)
+hastighet = 50  # Setter hastighet. (Sett en hastighet mellom -255 og 255)
+
+print("Velkommen til Lego BrickPi konkurransen! \n Kontrollene er som følger: \n w + enter: "
+      "fremover \n s + enter: revers \n d + enter: høyre \n a + enter: venstre \n x + enter: stopp"
+      "\n -------------------------- \n q + enter: avslutt program")
+
 while True:
     inp = str(raw_input())  # Take input from the terminal
+
     # Move the bot
-    if inp == 'w':
+    if inp.startswith("w"):
         forover()
-    elif inp == 'a':
+    elif inp.startswith("a"):
         venstre()
-    elif inp == 'd':
+    elif inp.startswith("d"):
         hoyre()
-    elif inp == 's':
+    elif inp.startswith("s"):
         revers()
-    elif inp == 'x':
+    elif inp.startswith("x"):
         stop()
+    elif inp.startswith("q"):
+        sys.exit()
     BrickPiUpdateValues()  # Update the motor values
 
     time.sleep(.01)  # sleep for 10 ms
